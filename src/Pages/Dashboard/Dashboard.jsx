7f03629c5f51abf3ff0a92371d27a563820aaa66
diahfani <diahaufaarini5@gmail.com>
2021-12-23 21:52:30 +0700

    add card statistik

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import TopNavbar from  '../../Components/Navbar/TopNavbar';
+import TopNavbar from '../../Components/Navbar/TopNavbar';
+import { Container } from 'react-bootstrap'
+import CardStatistik from '../../Components/CardStatistik/CardStatistik';
+
 function Dashboard(props) {
     return (
         <div>
-            <TopNavbar/>
-            <p style={{marginTop:"200px"}}>halo Dashboard</p>
+            <TopNavbar />
+            {/* <Container> */}
+                <CardStatistik/>
+            {/* </Container> */}
+            <Container>
+                <p style={{ marginTop: "200px" }}>halo Dashboard</p>
+            </Container>
+
         </div>
     );
 }
